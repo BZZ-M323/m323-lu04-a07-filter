c72dabe133bbd4c0698933055714b951496e3e50
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-15 11:25:18 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def calculate():
+def filter_even_numbers(numbers):
     """
-    calculates the total
-    :return: None
+    Filtert die geraden Zahlen aus der gegebenen Liste.
+    Args:
+    - numbers (list): Eine Liste von Zahlen.
+    Returns:
+    - even_numbers_list: Eine Liste der geraden Zahlen.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    # Ihr Code hier
+    return even_numbers_list
 
 
 if __name__ == '__main__':
-    calculate()
+    numbers = list(range(1, 51))
+    even_numbers = filter_even_numbers(numbers)
+    print(list(even_numbers))
--- main_test.py
+++ main_test.py
@@ -1,6 +1,8 @@
-import main
+from main import filter_even_numbers
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_filter_even_numbers():
+    assert filter_even_numbers(list(range(1, 51))) == list(range(2, 51, 2))  # Testen von 1 bis 50
+    assert filter_even_numbers([]) == []  # Testen einer leeren Liste
+    assert filter_even_numbers([1, 3, 5, 7]) == []  # Liste ohne gerade Zahlen
+    assert filter_even_numbers([2, 4, 6, 8]) == [2, 4, 6, 8]  # Liste nur mit geraden Zahlen
+    assert filter_even_numbers([1, 2, 3, 4, 5]) == [2, 4]  # Gemischte Liste
